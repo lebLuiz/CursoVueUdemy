3b473da3afc8fe19f8175ea52f3828d14c322b86
lebLuiz <leb.e.b.@hotmail.com>
2021-03-03 21:32:24 -0300

    Matador de Monstros

---
+++ matador-de-monstros/README.md
@@ -0,0 +1,6 @@
+## matador-de-monstros
+### eu-fiz
+Projeto feito por mim antes de começar o capítulo "`Matador de Monstros`". Observei inicialmente como era e logo comecei a fazer.
+
+### cara-do-curso-fez
+Projeto que o instrutor do curso fez durante o capítulo "`Matador de Monstros`".
--- matador-de-monstros/eu-fiz/app.js
+++ matador-de-monstros/eu-fiz/app.js
@@ -2,15 +2,11 @@ new Vue({
     el: '#projeto',
     data: {
         iniciar: false,
-
         vidaP: 100,
         vidaM: 100,
-
         listaPontuacao: [],
-
         msgGanhador: null,
     },
-
     computed: {
         barra1(){
             return {
@@ -19,7 +15,6 @@ new Vue({
                 height: '20px',
             }
         },
-
         barra2(){
             return {
                 backgroundColor: this.vidaM <= 20?'red':'green',
@@ -28,7 +23,6 @@ new Vue({
             }
         },
     },
-
     watch: {
         vidaP: function(v) {
             if(v <= 0 && this.vidaM > 0){
@@ -57,17 +51,13 @@ new Vue({
             }
         }
     },
-
     methods: {
         iniciarJogo(){
             this.iniciar = true;
-
             this.vidaP = 100;
             this.vidaM = 100;
-
             this.listaPontuacao = [];
         },
-
         gerarForça(min, max) {
             min = Math.ceil(min);
             max = Math.floor(max);
@@ -76,75 +66,59 @@ new Vue({
         ataque(){
             let danoP = this.gerarForça(1, 14);
             let danoM = this.gerarForça(1, 10);
-
             this.vidaP -= danoP;
             this.vidaM -= danoM;
-
             let resultadoM = {
                 individuo: 'MONSTRO',
                 acao: 'ATINGIU',
                 ponto: danoP,
             }
-
             let resultadoP = {
                 individuo: 'JOGADOR',
                 acao: 'ATINGIU',
                 ponto: danoM,
             }
-
             this.listaPontuacao.unshift(resultadoM, resultadoP);
         },
         ataqueEspecial(){
             let danoP = this.gerarForça(1, 14);
             let danoM = this.gerarForça(1, 20);
-
             this.vidaP -= danoP;
             this.vidaM -= danoM;
-
             let resultadoM = {
                 individuo: 'MONSTRO',
                 acao: 'ATINGIU',
                 ponto: danoP,
             }
-
             let resultadoP = {
                 individuo: 'JOGADOR',
                 acao: 'ATINGIU',
                 ponto: danoM,
             }
-            
             this.listaPontuacao.unshift(resultadoM, resultadoP);
         },
         curar(){
             let ganho = this.gerarForça(0, 13);
             let perda = this.gerarForça(0, 12);
-
             this.vidaP += ganho;
-
             let resultadoM = {
                 individuo: 'MONSTRO',
                 acao: 'ATINGIU',
                 ponto: perda,
             }
-
             let resultadoP = {
                 individuo: 'JOGADOR',
                 acao: 'GANHOU FORÇA',
                 ponto: ganho,
             }
-
             this.listaPontuacao.unshift(resultadoM, resultadoP);
             this.vidaP -= perda;
         },
         desistir(){
             this.iniciar = false;
-
             this.vidaP = 100;
             this.vidaM = 100;
-
             this.listaPontuacao = [];
         }
     }
-
-
 })
