869f5c35d96ae4c03f7648cc1c065e39231644e5
lebLuiz <leb.e.b.@hotmail.com>
2021-03-23 00:23:22 -0300

    Incrementos a mais

---
+++ vue-cli/projeto-simples/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    {{ titulo }}
+    <button @click="titulo += '#'">Alterar</button>
+  </div>
+</template>
+
+<script>
+export default {
+
+  data(){
+    return {
+      titulo: 'Teste Data usando Vue.js!!',
+    }
+  },
+
+}
+</script>
+
+<style>
+  #app {
+    background-color: chocolate;
+    color: #fff;
+  }
+</style>
